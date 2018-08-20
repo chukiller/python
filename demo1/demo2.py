c231e7081778d5d0347e6a5456e40badb4897767
HZQ <643107836@qq.com>
2018-08-21 00:04:08 +0800

    INSERT DEMO TO MASTER

---
+++ demo1/demo2.py
@@ -0,0 +1,4 @@
+if not 3 > 2:
+    print("X")
+else:
+    print("O")
--- demo1/onegame.py
+++ demo1/onegame.py
@@ -1,8 +1,18 @@
 print("----------------------------")
-temp = input("输入一个数字：")
-guess = int(temp)
-if guess == 8:
-    print("O")
-else:
-    print("X")
+val = 8
+val2 = 3
+ret = val2
+inputStr = "请输入任意一个数字,机会只有%s次哦：" % ret
+guess = input(inputStr)
+print(guess)
+while guess != val and ret > 0:
+    if guess == val:
+        print("猜对啦，游戏结束")
+    else:
+        if guess > val:
+            print("大了大了")
+        else:
+            print("小了小了")
+    temp = input(inputStr)
+    guess = int(temp)
 print("----------------------------")
