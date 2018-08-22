0c1d1c40c89d0a05a14f0061d34cce0a8263084c
HZQ <643107836@qq.com>
2018-08-22 14:33:26 +0800

    UPDATE GAME.PY TO MASTER

--- demo1/onegame.py
+++ demo1/onegame.py
@@ -1,18 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import random
+
 print("----------------------------")
-val = 8
-val2 = 3
-ret = val2
-inputStr = "请输入任意一个数字,机会只有%s次哦：" % ret
-guess = input(inputStr)
-print(guess)
+val = random.randint(1,10)
+ret = 3
+guess = int(input("请输入任意一个数字,机会只有%s次哦：" % ret))
+
 while guess != val and ret > 0:
+    ret-=1
     if guess == val:
         print("猜对啦，游戏结束")
-    else:
+    elif guess != val and ret > 0:
+        print("玩家出: %s" % guess,"电脑出: %s" % val)
         if guess > val:
             print("大了大了")
         else:
             print("小了小了")
-    temp = input(inputStr)
-    guess = int(temp)
+        val = random.randint(1,10)
+        guess = int(input("请输入任意一个数字,机会只有%s次哦：" % ret))
+    else:
+        print("\n------------------")
+        print("|没次数了，请充值|")
+        print("------------------\n")
 print("----------------------------")
